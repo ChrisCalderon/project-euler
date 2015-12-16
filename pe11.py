96055343fc3ef7653184708fe350018ee751ea17
Christian Calderon <calderon.christian760@gmail.com>
2015-12-15 21:09:27 -0800

    Episode 4, worked on number 11, still unsolved!

--- pe11.py
+++ pe11.py
@@ -1,28 +1,59 @@
-def yield_next_product(ints, n):
-    '''This will find the left-right and up-down products.'''
-    # ints is a list
-    while len(ints) > n:
-        next_sequence = ints[:n]
-        
-        try:
-            zero = next_sequence.index(0)
-        except:
-            zero = -1
+def transpose(matrix):
+    return map(list, zip(*matrix))
 
-        if zero > -1:
-            ints = ints[zero + 1:]
-        else:
-            yield reduce(lambda a, b: a*b, next_sequence, 1)
-            ints = ints[1:]
+def yield_horizontal_products(ints, n):
+    for i in range(len(ints)):
+        for j in range(len(ints) - n + 1):
+            product = 1
+            for k in range(n):
+                if ints[i][j+k]:
+                    product *= ints[i][j+k]
+                else:
+                    break
+            else:
+                yield product
+        print ints[i][j]
 
-def find_diagonal_products(ints, n):
-    pass
-
+def yield_diagonal_products(ints, n):
+    for i in range(len(ints) - n + 1):
+        for j in range(len(ints) - n + 1):
+            product = 1
+            for k in range(n):
+                if ints[i+k][j+k]:
+                    product *= ints[i+k][j+k]
+                else:
+                    break
+            else:
+                yield product
+        print ints[i][j]
+                
 def find_largest_adjacent_product(ints, n):
     # ints is an array of arrays
-    left_right = max(yield_next_product(sum(ints, []), n))
-    up_down = max(yield_next_product(sum(zip(*ints), []), n))
+    print 'left'
+    left = max(yield_horizontal_products(ints, n))
+    print 'right'
+    right = max(yield_horizontal_products(map(lambda row: list(reversed(row)),
+                                              ints),
+                                          n))
+    print 'up'
+    up = max(yield_horizontal_products(transpose(ints), n))
+    print 'down'
+    down = max(yield_horizontal_products(map(lambda row: list(reversed(row)),
+                                             transpose(ints)),
+                                         n))
+    print 'diagonal'
+    diagonal = max(yield_diagonal_products(ints, n))
+    return max((left, right, up, down, diagonal))
     # the zip(*ints) transposes the array of arrays
-
     
+def main():
+    intsFile = open('pe11.txt')
+    ints = map(lambda line: map(int, line.split(' ')),
+               filter(None,
+                      intsFile.read().split('\n')))
+    print ints
+    n = 4
+    print find_largest_adjacent_product(ints, n)
     
+if __name__ == '__main__':
+    main()
